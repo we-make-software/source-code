9fe230958eb409b8cc8059bd20295b2d6835ca82
Pirasath Luxchumykanthan <hello@how-do-i-get-your-attention.com>
2025-03-09 23:29:24 +0100

    Update TheRequirements.h

--- TheRequirements/TheRequirements.h
+++ TheRequirements/TheRequirements.h
@@ -5,4 +5,10 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 
+#define Setup(init, exit) \
+    static int __init init_module_func(void) { init return 0;} \
+    static void __exit cleanup_module_func(void) { exit(); } \
+    module_init(init_module_func); \
+    module_exit(cleanup_module_func);
+
 #endif // TheRequirements_H
