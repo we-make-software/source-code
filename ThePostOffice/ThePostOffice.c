076f731f5ad0761b4aea90a1858d24d4a984284e
root <root@B873EE7>
2025-03-13 23:17:24 +0000

    Auto-update: Thu Mar 13 11:17:24 PM UTC 2025

--- ThePostOffice/ThePostOffice.c
+++ ThePostOffice/ThePostOffice.c
@@ -5,8 +5,6 @@
 #include <linux/ip.h>
 #include <linux/tcp.h>
 #include <linux/ipv6.h>
-
-//This is the expiry work base
 struct ExpiryWorkBase {
     struct mutex Mutex;
     struct delayed_work Work;
@@ -27,6 +25,7 @@ static void SetExpiryWorkBase(struct ExpiryWorkBase *expiry) {
 }
 static void ProcessExpiryWorkBaseToDo(struct work_struct *work) {
     struct ExpiryWorkBase *expiry = container_of(work, struct ExpiryWorkBase, Work.work);
+    if(!expiry)return;
     mutex_lock(&expiry->Mutex);
     if(!expiry->Cancel) {
         mutex_unlock(&expiry->Mutex);
@@ -37,10 +36,9 @@ static void ProcessExpiryWorkBaseToDo(struct work_struct *work) {
     mutex_lock(&expiry->Mutex);
     if(expiry->CallExpiryWorkFunction)
        expiry->CallExpiryWorkFunction(expiry->Parent);  
-    if(expiry){
-        destroy_workqueue(expiry->Workqueue);
-        kfree(expiry->Parent);
-    }    
+    destroy_workqueue(expiry->Workqueue);
+    kfree(expiry->Parent);
+    expiry=NULL;  
 }
 static void SetupExpiryWorkBase(struct ExpiryWorkBase *expiry, void (*CallExpiryWorkFunction)(void*), void*Parent) {
     expiry->CallExpiryWorkFunction=CallExpiryWorkFunction;
@@ -49,8 +47,8 @@ static void SetupExpiryWorkBase(struct ExpiryWorkBase *expiry, void (*CallExpiry
     expiry->Workqueue=create_singlethread_workqueue("ExpiryWorkBase");
     mutex_init(&expiry->Mutex);
     INIT_DELAYED_WORK(&expiry->Work, ProcessExpiryWorkBaseToDo);
+    SetExpiryWorkBase(expiry);
 }
-// Start the ForceClose process the real start its in CleanupNetworkAdapters
 static void ForceCloseExpiryWorkBase(struct ExpiryWorkBase *expiry) {
     mutex_lock(&expiry->Mutex);
     if (expiry->Cancel) {
@@ -65,7 +63,6 @@ static void ForceCloseExpiryWorkBase(struct ExpiryWorkBase *expiry) {
     }
     mutex_unlock(&expiry->Mutex);
 }
-
 struct Router;
 struct NetworkAdapter {
     struct Router *Routers;
@@ -73,7 +70,6 @@ struct NetworkAdapter {
     struct NetworkAdapter *Next, *Prev;
     struct ExpiryWorkBase Expiry;
 };
-
 struct VersionNetworkSegment;
 struct Router{
     struct NetworkAdapter *NetworkInterfaces;
@@ -91,93 +87,81 @@ static void CleanupNetworkAdapters(void){
         return;
     }
     struct NetworkAdapter*CurrentAdapter=NetworkAdapters;
-    for(;CurrentAdapter;CurrentAdapter=CurrentAdapter->Prev)
+    while(CurrentAdapter){
+        struct NetworkAdapter*PrevAdapter=CurrentAdapter->Prev;
         ForceCloseExpiryWorkBase(&CurrentAdapter->Expiry);
+        CurrentAdapter=PrevAdapter;
+    }
     mutex_unlock(&NetworkAdaptersMutex);
 }
-
-
 static void DeleteNetworkAdapter(void*NetworkAdapter){
-    struct NetworkAdapter *CurrentAdapter=(struct NetworkAdapter*)NetworkAdapter;
+    struct NetworkAdapter*CurrentAdapter=(struct NetworkAdapter*)NetworkAdapter;
     mutex_lock(&CurrentAdapter->Expiry.Mutex);
     struct Router *CurrentRouter=CurrentAdapter->Routers;
-    for(;CurrentRouter;CurrentRouter=CurrentRouter->Prev)
+    while (CurrentRouter) {
+        struct Router*PrevRouter=CurrentRouter->Prev;
         ForceCloseExpiryWorkBase(&CurrentRouter->Expiry);
+        CurrentRouter=PrevRouter;
+    }
     if(CurrentAdapter->Next)CurrentAdapter->Next->Prev=CurrentAdapter->Prev;
     if(CurrentAdapter->Prev)CurrentAdapter->Prev->Next=CurrentAdapter->Next;
     if(NetworkAdapters==CurrentAdapter)NetworkAdapters=CurrentAdapter->Prev?CurrentAdapter->Prev:CurrentAdapter->Next;
     mutex_unlock(&CurrentAdapter->Expiry.Mutex);
 }
-
 static struct Router *Routers=NULL;
-static void HelpFunctionForGetRouterSetExpiryWorkBase(struct Router*router){
-    SetExpiryWorkBase(&router->Expiry);
-    SetExpiryWorkBase(&router->NetworkInterfaces->Expiry);
+
+
+static struct VersionNetworkSegment *Segments=NULL;
+
+
+//This is segment of network
+
+enum VersionType { Version4, Version6 };
+enum SegmentType { Server, Client };
+struct VersionNetworkSegment{
+    struct Router *router;
+    enum VersionType Is; 
+    struct ExpiryWorkBase Expiry;
+    enum SegmentType Type; 
+    struct VersionNetworkSegment *Next, *Prev;
+    
+};
+struct Version4NetworkSegment {
+    struct Router *router;
+    enum VersionType Is; 
+    struct ExpiryWorkBase Expiry;
+    enum SegmentType Type; 
+    struct Version4NetworkSegment *Next, *Prev; 
+    u32 Address;
+};
+struct Version6NetworkSegment{
+    struct Router *router;
+    enum VersionType Is; 
+    struct ExpiryWorkBase Expiry;
+    enum SegmentType Type; 
+    struct Version6NetworkSegment*Next,*Prev;
+    u64 AddressHigh;
+    u64 AddressLow;
+};
+void FreeNetworkSegment(struct VersionNetworkSegment *segment) {
+    if(!segment)return;
+    if(segment->Is==Version6) 
+        kfree((struct Version6NetworkSegment*)segment);
+    else 
+        kfree((struct Version4NetworkSegment*)segment);
 }
 
-//i made this if other want to do something inside the router i dont know. mabe something.
+static void DeleteNetworkSegment(void*segment){
+//for the client
+}
+
+static void DeleteRouterNetworkSegment(void*segment){
+//for the router
+}
 
 static void DeleteRouter(void*router){
 
 }
-
-//This is segment of network
-static struct VersionNetworkSegment* HelpFunctionForNetworkSegmentByRouter(struct VersionNetworkSegment*CurrentSegment,struct Router*router);
-struct VersionNetworkSegment{
-    struct Router *router;
-    u16*Gateway;
-    bool IsVersion6;
-    struct ExpiryWorkBase Expiry;
-};
-
-//This is the segment of network for version 4
-struct Version4NetworkSegmentLast {
-    struct Router *router;
-    u16*Gateway;
-    bool IsVersion6;
-    struct ExpiryWorkBase Expiry;
-    u16 Address;
-    struct Version4NetworkSegmentLast *Next, *Prev; 
-};
-struct Version4NetworkSegmentFirst {
-    u16 Address;
-    struct Version4NetworkSegmentLast *Last;
-    struct Version4NetworkSegmentFirst *Next,*Prev;
-    struct ExpiryWorkBase Expiry;
-};
-static struct Version4NetworkSegmentFirst *Version4NetworkSegmentFirst=NULL;
-static DEFINE_MUTEX(Version4NetworkSegmentMutex);
-
-//This is the segment of network for version 6
-struct Version6NetworkSegmentLast
-{   struct Router *router;
-    u16*Gateway;
-    bool IsVersion6;
-    struct ExpiryWorkBase Expiry;
-    u64 Address;
-    struct Version6NetworkSegmentLast *Next,*Prev;
-};
-struct Version6NetworkSegmentFirst {
-    u64 Address;
-    struct Version6NetworkSegmentLast *Last;
-    struct Version6NetworkSegmentFirst *Next,*Prev;
-    struct ExpiryWorkBase Expiry;
-};
-static struct Version6NetworkSegmentFirst *Version6NetworkSegmentFirst=NULL;
-static DEFINE_MUTEX(Version6NetworkSegmentMutex);
-
-// Mix function for the segment of network
-static void HelpFunctionForNetworkSegmentByRouterSetExpiryWorkBase(struct VersionNetworkSegment*CurrentSegment){
-    if(!CurrentSegment)return;
-    SetExpiryWorkBase(&CurrentSegment->Expiry);
-    HelpFunctionForGetRouterSetExpiryWorkBase(CurrentSegment->router);
-}
-
-
-;
-//Server
-static struct VersionNetworkSegment*Server=NULL,*Client=NULL;
-
 //This is the packet Receive function
 static int ThePostOfficeReceivePacket(struct sk_buff*skb,struct net_device*dev,struct packet_type*pt,struct net_device*orig_dev){
     if(!strcmp(dev->name,"lo")==0||skb->len<14||skb->pkt_type==PACKET_OUTGOING)
@@ -185,10 +169,9 @@ static int ThePostOfficeReceivePacket(struct sk_buff*skb,struct net_device*dev,s
     u8*data;
     data=skb_mac_header(skb);
     if((data[0]&2))return 0;
-    struct NetworkAdapter *NetworkAdapter=NULL;
+    struct NetworkAdapter *NetworkAdapter=NetworkAdapters;
     mutex_lock(&NetworkAdaptersMutex);
-    for(NetworkAdapter=NetworkAdapters;NetworkAdapter;NetworkAdapter=NetworkAdapter->Next)
-        if(NetworkAdapter->dev==dev)break;
+    for(;NetworkAdapter&&NetworkAdapter->dev!=dev;NetworkAdapter=NetworkAdapter->Next);
     mutex_unlock(&NetworkAdaptersMutex);
     if(!NetworkAdapter){
         NetworkAdapter=kmalloc(sizeof(struct NetworkAdapter),GFP_KERNEL);
@@ -197,7 +180,7 @@ static int ThePostOfficeReceivePacket(struct sk_buff*skb,struct net_device*dev,s
             return 0;
         }
         NetworkAdapter->dev=dev;
-        mutex_init(&NetworkAdapter->Expiry.Mutex);
+        SetupExpiryWorkBase(&NetworkAdapter->Expiry,DeleteNetworkAdapter,NetworkAdapter);
         NetworkAdapter->Next=NULL;
         NetworkAdapter->Routers=NULL;
         mutex_lock(&NetworkAdaptersMutex);
@@ -205,12 +188,10 @@ static int ThePostOfficeReceivePacket(struct sk_buff*skb,struct net_device*dev,s
         NetworkAdapter->Next=NetworkAdapters;
         NetworkAdapters=NetworkAdapter;
         mutex_unlock(&NetworkAdaptersMutex);
-        SetupExpiryWorkBase(&NetworkAdapter->Expiry,DeleteNetworkAdapter,NetworkAdapter);
-    }else 
-        SetExpiryWorkBase(&NetworkAdapter->Expiry);
-    struct Router*router=NULL;
-    for(router=NetworkAdapter->Routers;router;router=router->Next)
-        if(!memcmp(router->MediaAccessControl,data+6,6))break;
+    } 
+    struct Router*router=NetworkAdapter->Routers;
+    for(;router&&memcmp(router->MediaAccessControl,data+6,6)!=0;router=router->Next);
+      
     if(!router){
         mutex_lock(&NetworkAdapter->Expiry.Mutex);
         router=kmalloc(sizeof(struct Router),GFP_KERNEL);
@@ -222,17 +203,17 @@ static int ThePostOfficeReceivePacket(struct sk_buff*skb,struct net_device*dev,s
         memcpy(router->MediaAccessControl,data+6,6);
         router->NetworkInterfaces=NetworkAdapter;
         router->Next=NULL;
+        router->Segments=NULL;
+        SetupExpiryWorkBase(&router->Expiry,DeleteRouter,router);
         router->Prev=NetworkAdapter->Routers;
         if(NetworkAdapter->Routers)NetworkAdapter->Routers->Next=router;
         NetworkAdapter->Routers=router;
-        SetupExpiryWorkBase(&router->Expiry,DeleteRouter,router);
         mutex_unlock(&NetworkAdapter->Expiry.Mutex);
-    }else 
-        SetExpiryWorkBase(&router->Expiry);
+    }
     bool IsTransmissionControlProtocol=false;
     u16 ethertype=ntohs(*(u16*)(data+12)),SourcePort=0,
         DestinationPort=0;
-    struct VersionNetworkSegment *Server=NULL,*Client=NULL;
+    struct VersionNetworkSegment *server=NULL,*client=NULL;
     switch(ethertype){
         case 2048:{
             if(data[14]!=69){
@@ -240,14 +221,113 @@ static int ThePostOfficeReceivePacket(struct sk_buff*skb,struct net_device*dev,s
                 return 1;
             }
             SourcePort=ntohs(*(u16*)(data+36));
-            const u16* address = (const u16*)(data + 26);
             if((IsTransmissionControlProtocol=(data[23]==6))&&SourcePort==22)return 0;
-            if(!(Server=GetVersion4NetworkSegmentByRouter((const u16*)(data + 26),router))){
-                //here wee create it this is server ip addres
+            u32 SourceIP=*(u32*)(data+26);
+            if(!router->Segments){
+                router->Segments=kmalloc(sizeof(struct Version4NetworkSegment),GFP_KERNEL);
+                if(!router->Segments){
+                    kfree_skb(skb);
+                    return 1;
+                }
+                struct Version4NetworkSegment*segment=(struct Version4NetworkSegment*)router->Segments;
+                segment->router=router;
+                segment->Is=Version4;
+                segment->Type=Server;
+                segment->Address=SourceIP;
+                segment->Prev=segment->Next=NULL;
+                SetupExpiryWorkBase(&segment->Expiry,DeleteRouterNetworkSegment,segment);
+                server=segment;
+            }else{
+                struct Version4NetworkSegment*segment=(struct Version4NetworkSegment*)router->Segments;
+                struct Version4NetworkSegment*last=NULL;
+                for(;segment&&(segment->Is==Version6||(segment->Next&&segment->Next->Address<=SourceIP));segment=segment->Next){
+                    if(segment->Is==Version4) last=segment;
+                    if(segment->Is==Version4&&segment->Address==SourceIP){
+                        server=segment;
+                        break;
+                    }
+                }
+                if(!server){
+                    struct Version4NetworkSegment*new=kmalloc(sizeof(struct Version4NetworkSegment),GFP_KERNEL);
+                    if(!new){
+                        kfree_skb(skb);
+                        return 1;
+                    }
+                    new->router=router;
+                    new->Is=Version4;
+                    new->Type=Server;
+                    new->Address=SourceIP;
+                    new->Prev=new->Next=NULL;
+                    SetupExpiryWorkBase(&new->Expiry,DeleteRouterNetworkSegment,new);
+                    if(!last){
+                        new->Next=(struct Version4NetworkSegment*)router->Segments;
+                        if(router->Segments)router->Segments->Prev=new;
+                        router->Segments=(struct VersionNetworkSegment*)new;
+                    }else{
+                        new->Next=last->Next;
+                        new->Prev=last;
+                        if(last->Next)last->Next->Prev=new;
+                        last->Next=new;
+                    }
+                    server=new;    
+                }
             }
-            if(!(Client=GetVersion4NetworkSegmentByRouter((const u16*)(data+30),router))){
-                //here wee create it this is client ip addres
+            u32 DestinationIP=*(u32*)(data+30);
+            if(!Segments){
+                Segments=kmalloc(sizeof(struct Version4NetworkSegment),GFP_KERNEL);
+                if(!Segments){
+                    kfree_skb(skb);
+                    return 1;
+                }
+                struct Version4NetworkSegment*segment=(struct Version4NetworkSegment*)Segments;
+                segment->router=NULL;
+                segment->Is=Version4;
+                segment->Type=Client;
+                segment->Address=DestinationIP;
+                segment->Prev=segment->Next=NULL;
+                SetupExpiryWorkBase(&segment->Expiry,DeleteRouterNetworkSegment,segment);
+                client=segment;
+            }else{
+                struct Version4NetworkSegment*segment=(struct Version4NetworkSegment*)Segments;
+                struct Version4NetworkSegment*last=NULL;
+                for(;segment&&(segment->Is==Version6||(segment->Next&&segment->Next->Address<=DestinationIP));segment=segment->Next){
+                    if(segment->Is==Version4) last=segment;
+                    if(segment->Is==Version4&&segment->Address==DestinationIP){
+                        client=segment;
+                        break;
+                    }
+                }
+                if(!client){
+                    struct Version4NetworkSegment*new=kmalloc(sizeof(struct Version4NetworkSegment),GFP_KERNEL);
+                    if(!new){
+                        kfree_skb(skb);
+                        return 1;
+                    }
+                    new->router=NULL;
+                    new->Is=Version4;
+                    new->Type=Client;
+                    new->Address=DestinationIP;
+                    new->Prev=new->Next=NULL;
+                    SetupExpiryWorkBase(&new->Expiry,DeleteRouterNetworkSegment,new);
+                    if(!last){
+                        new->Next=(struct Version4NetworkSegment*)Segments;
+                        if(Segments)Segments->Prev=new;
+                        Segments=(struct VersionNetworkSegment*)new;
+                    }else{
+                        new->Next=last->Next;
+                        new->Prev=last;
+                        if(last->Next)last->Next->Prev=new;
+                        last->Next=new;
+                    }
+                    client=new;
+                }
             }
+            
+            
+            SetExpiryWorkBase(&server->Expiry);// when its this need to do thats the point
+            SetExpiryWorkBase(&router->Expiry);// when its this need to do thats the point
+            SetExpiryWorkBase(&NetworkAdapter->Expiry);// when its this need to do thats the point
+
             DestinationPort=ntohs(*(u16*)(data+34));    
             break;
         }
@@ -255,12 +335,12 @@ static int ThePostOfficeReceivePacket(struct sk_buff*skb,struct net_device*dev,s
             SourcePort=ntohs(*(u16*)(data+56));
             if((data[22]==254&&(data[23]&192)==128)||(data[38]==254&&(data[39]&192)==128))return 0;
             if((IsTransmissionControlProtocol=(data[20]==6))&&SourcePort==22)return 0;
-            if(!(Server=GetVersion6NetworkSegmentByRouter((const u16*)(data+22),router))){
-                //here wee create it this is server ip addres
-            }
-            if(!(Client=GetVersion6NetworkSegmentByRouter((const u16*)(data+38),router))){
-                //here wee create it this is client ip addres
-            }           
+            u64 SourceIPHigh=*(u64*)(data+22);
+            u64 SourceIPLow=*(u64*)(data+30);
+            u64 DestinationIP_High=*(u64*)(data+38);
+            u64 DestinationIP_Low=*(u64*)(data+46);
+
+           
             DestinationPort=ntohs(*(u16*)(data+54));
             break;
         }
--- ThePostOffice/ThePostOffice.h
+++ ThePostOffice/ThePostOffice.h
@@ -1,29 +1,5 @@
 #ifndef ThePostOffice_h
 #define ThePostOffice_h
 #include "../TheRequirements/TheRequirements.h"
-struct VersionNetworkSegment{
-    struct Router *router;
-    u16*Address;
-    bool IsVersion6;
-    struct VersionNetworkSegment *Next,*Prev;
-};
-struct Router {
-    struct NetworkAdapter *NetworkInterfaces;
-    u8 MediaAccessControl[6];
-    struct VersionNetworkSegment *Segments;
-};
-struct NetworkAdapter {
-    struct Router *Routers;
-};
-extern void GetNetworkAdapter(const u64 MediaAccessControl, void (*Callback)(struct NetworkAdapter*));
-extern void GetAllNetworkAdapters(void(*Callback)(struct NetworkAdapter*));
-extern struct Router *GetRouter(const u8*MediaAccessControl, const void(*Callback)(struct Router*));
-extern void GetAllRouters(const struct NetworkAdapter *NetworkInterfaces, void(*Callback)(struct Router*));
-extern struct VersionNetworkSegment *GetVersionNetworkSegment(const u16*Address, const bool IsVersion6);
-extern struct VersionNetworkSegment *GetVersion6NetworkSegment(const u16*Address);
-extern struct VersionNetworkSegment *GetVersion4NetworkSegment(const u16*Address);
-extern struct VersionNetworkSegment *GetVersion6NetworkSegmentByRouter(const u16*Address, struct Router*router);
-extern struct VersionNetworkSegment *GetVersionNetworkSegmentByRouter(const u16*Address, struct Router*router, const bool IsVersion6);
-
 #endif
 
